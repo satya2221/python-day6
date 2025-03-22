eb9506bb7aa0da3ffa8d6d4a4f9528ec0fe0b614
satya2221 <satyaghifari12@gmail.com>
2025-03-22 16:48:34 +0700

    feat: Add actor on Todos

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from .utils import generate_id
+from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
     id = models.CharField(primary_key=True, default=generate_id, editable=False, max_length=100)
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True) #one to many dengan 1 user banyak record
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
